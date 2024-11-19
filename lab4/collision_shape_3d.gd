32c1a9b80f3939b03e2dc2b3edb0ae91cb5ae819
nguyenlea1 <hnguyen94@fordham.edu>
2024-11-19 14:40:48 -0500

    script

--- lab4/collision_shape_3d.gd
+++ lab4/collision_shape_3d.gd
@@ -8,21 +8,16 @@ func _physics_process(delta):
 
 	var input_vector = Vector3.ZERO
 
-
 	if Input.is_action_pressed("move_forward"): 
-
 		input_vector.z += 1
 
 	if Input.is_action_pressed("move_backward"): 
-
 		input_vector.z -= 1
 
 	if Input.is_action_pressed("move_left"): 
-
 		input_vector.x -= 1
 
 	if Input.is_action_pressed("move_right"): 
-
 		input_vector.x += 1
 
 
