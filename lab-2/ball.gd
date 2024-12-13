ec4e5fb5aeb159bd760503127617c6882c799821
nguyenlea1 <hnguyen94@fordham.edu>
2024-12-13 16:28:17 -0500

    resubmitting lab2

--- lab-2/ball.gd
+++ lab-2/ball.gd
@@ -1,6 +1,11 @@
 extends Sprite2D
 
 @export var speed = 1
+@export var progress_bar: ProgressBar
+@export var name_of_variable = 2
+var thing_to_test = false
+var health = 5  
+# Declare health variable
 # Called when the node enters the scene tree for the first time.
 func _ready() :
 	pass# Replace with function body.
@@ -13,12 +18,13 @@ func _process(delta):
 		position.x = position.x + speed
 	if Input.is_action_pressed("ui_left"):
 		position.x -= speed
-	
 	if Input.is_action_pressed("ui_up"):
 		position.y -= speed
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed
-
-@export var name_of_variable = 2
-var thing_to_test = false
-var health = 5
+		
+	if health <= 2:  # Check if health is low
+		print("Warning: Health is low!")
+# You can add any other action here, like updating a UI element or triggering an event
+	else:
+		print("Health is sufficient.")
