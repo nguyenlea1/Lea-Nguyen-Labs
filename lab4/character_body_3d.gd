8d9f61667e2fd598971dbcc3ac6772fa194c431c
nguyenlea1 <hnguyen94@fordham.edu>
2024-12-13 16:34:27 -0500

    change ui input

--- lab4/character_body_3d.gd
+++ lab4/character_body_3d.gd
@@ -9,16 +9,13 @@ func _physics_process(delta: float) -> void:
 
 	var input_vector = Vector3.ZERO
 
-	if Input.is_action_pressed("move_forward"): 
+	if Input.is_action_pressed("ui_up"): 
 		input_vector.z += 1
-
-	if Input.is_action_pressed("move_backward"): 
+	if Input.is_action_pressed("ui_down"): 
 		input_vector.z -= 1
-
-	if Input.is_action_pressed("move_left"): 
+	if Input.is_action_pressed("ui_left"): 
 		input_vector.x -= 1
-
-	if Input.is_action_pressed("move_right"): 
+	if Input.is_action_pressed("ui_right"): 
 		input_vector.x += 1
 
 
